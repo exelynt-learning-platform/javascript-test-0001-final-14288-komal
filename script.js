4e7cd906a8a267a4dde9bad220e08fb896069c49
Komal6627 <komalkilledar1996@gmail.com>
2026-03-16 23:26:16 +0530

    update changes

--- script.js
+++ script.js
@@ -1,34 +1,37 @@
-let n = 4
+let n = 5;
 
-for(let i = 1; i<=n;i++){
-    let rows = ""
-    for(let s=1; s<=n-i;s++){
-        rows += " "
+for (let i = 1; i <= n; i++) {
+  let row = "";
+
+  for (let s = 1; s <= n - i; s++) {
+    row += " ";
+  }
+
+  for (let j = 1; j <= (2 * i - 1); j++) {
+    if (j === 1 || j === (2 * i - 1)) {
+      row += "*";
+    } else {
+      row += " ";
     }
-    for(let j = 1; j<=i;j++){
-        if(i === 1 || j === 1 || j ===i){
-                rows += "* "
-        }
-        else{
-            rows += "  "
-        }
-    }
-    console.log(rows)
+  }
+
+  console.log(row);
 }
 
-for(let i = n-1; i >= 1;i--){
-    let rows = ""
-    for(let s=1; s<=n-i;s++){
-        rows += " "
-    }
-    for(let j = 1; j<=i;j++){
-        if(i === 1 || j === 1 || j ===i){
-                rows += "* "
-        }
-        else{
-            rows += "  "
-        }
-    }
-    console.log(rows)
-}
+for (let i = n - 1; i >= 1; i--) {
+  let row = "";
 
+  for (let s = 1; s <= n - i; s++) {
+    row += " ";
+  }
+
+  for (let j = 1; j <= (2 * i - 1); j++) {
+    if (j === 1 || j === (2 * i - 1)) {
+      row += "*";
+    } else {
+      row += " ";
+    }
+  }
+
+  console.log(row);
+}
